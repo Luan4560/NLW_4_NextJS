3ebc0393caaa5d80c85cfb664cee568f49b14fb7
Luan Nascimento <luan.nascimento11@outlook.com>
2021-02-24 23:29:08 -0300

    Finished countdown

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,26 @@
+import Head from 'next/head'
 import { CompletedChallenges } from '../components/CompletedChallenges'
 import {ExperienceBar} from '../components/ExperienceBar'
 import {Profile} from '../components/Profile'
+import {Countdown} from '../components/Countdown'
 import styles from '../styles/Pages/Home.module.css'
 
 export default function Home() {
   return (
     <>
      <div className={styles.container}>
-      <ExperienceBar />
+
+     <Head>
+       <title>Início | Move.it</title>
+     </Head>
+
+    <ExperienceBar />
 
       <section>
         <div>
           <Profile />
           <CompletedChallenges/>
+          <Countdown/>
         </div>
         <div>
 
