b83f103e7d4bd3a5d23765bed4f55c552776e0a9
Luan Nascimento <luan.nascimento11@outlook.com>
2021-02-27 18:59:33 -0300

    Almost finished

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import {GetServerSideProps} from 'next' 
+
 import { CompletedChallenges } from '../components/CompletedChallenges'
 import {ExperienceBar} from '../components/ExperienceBar'
 import {Profile} from '../components/Profile'
@@ -8,7 +10,8 @@ import { ChallengeBox } from '../components/ChallengeBox'
 import styles from '../styles/Pages/Home.module.css'
 import { CountdownProvider } from '../contexts/CountdownContext'
 
-export default function Home() {
+export default function Home(props) {
+  console.log(props)
   return (
     <>
      <div className={styles.container}>
@@ -33,4 +36,16 @@ export default function Home() {
     </div>
     </>
   )
+}
+
+export const getServerSideProps:GetServerSideProps = async(ctx) => {
+  const {level, currentExperience, challengesCompleted} = ctx.req.cookies;
+
+  return {
+    props: {
+      level,
+      currentExperience,
+      challengesCompleted,
+    }
+  }
 }
